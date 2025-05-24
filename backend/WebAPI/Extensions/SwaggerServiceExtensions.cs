af669114b51a43e1b387cb387e8e53e373baa865
Rivemn <132005694+Rivemn@users.noreply.github.com>
2025-05-24 19:11:25 +0300

    basic project structure

---
+++ backend/WebAPI/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace DateSpaceWebAPI.Extensions
+{
+	public static class SwaggerServiceExtensions
+	{
+		public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+		{
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen(options =>
+			{
+				options.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = "DeskBooking API",
+					Version = "v1",
+					Description = "API documentation for DeskBookingApi"
+				});
+
+				options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Name = "Authorization",
+					Type = SecuritySchemeType.Http,
+					Scheme = "Bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Description = "Enter the JWT token in the format: Bearer {your token}"
+				});
+
+				options.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							}
+						},
+						new string[] {}
+					}
+				});
+			});
+
+			return services;
+		}
+
+		public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI(options =>
+			{
+				options.SwaggerEndpoint("/swagger/v1/swagger.json", "DeskBooking API v1");
+				options.RoutePrefix = string.Empty;
+			});
+
+			return app;
+		}
+	}
+}
---
+++ backend/WebAPI/Program.cs
@@ -0,0 +1,32 @@
+﻿using DateSpaceWebAPI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll", policy =>
+	{
+		policy.AllowAnyOrigin()
+			  .AllowAnyHeader()
+			  .AllowAnyMethod();
+	});
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerDocumentation();
+
+var app = builder.Build();
+
+app.UseSwaggerDocumentation();
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
